dee2b89919ffd1d01db0cb0dcf29644d3cbdc9df
AndreyLukashev <enrgy2024@internet.ru>
2023-12-18 23:03:08 +0300

    add search

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ import './src/components/typography/_typography.js';
 import './src/components/top-bar/top-bar.js';
 import './src/components/header/header.js';
 import './src/components/logo/logo.js';
+import './src/components/grid/grid.js';
+import './src/components/forms/inputs/search-input.js';
+import './src/components/button/button.js';
+import './src/components/forms/select/select.js';
